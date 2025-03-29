2babc22491c77352da607459b8098f769aeda51f
Ikul23 <ikul23@icloud.com>
2025-03-29 16:16:55 +0300

    DB added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
-use App\Http\Controllers\FormProcessor;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormProcessor;
+use App\Models\Employee;
 
 // Показ формы (GET)
 Route::get('/userform', [FormProcessor::class, 'index']);
 
 // Обработка формы (POST)
 Route::post('/store_form', [FormProcessor::class, 'store'])->name('form.store');
+
+// Тест работы с БД
+Route::get('/test_employees', function () {
+    $employees = App\Models\Employee::all();
+    return response()->json($employees);
+});
